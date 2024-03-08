c8c6d5dfee9610cec00ab513baf06701df92c6ca
Fares Rbd <140640835+Fares-Rbd@users.noreply.github.com>
2024-03-08 23:01:17 +0100

    Add files via upload
    Simple Spring Boot application that showcases the use of Dependency Injection, Inversion of Control and java Beans

---
+++ DependencyInjectionSpring/src/main/java/com/JEE/classes/TotoyaEngine.java
@@ -0,0 +1,43 @@
+package com.JEE.classes;
+import com.JEE.interfaces.IEngine;
+public class TotoyaEngine implements IEngine {
+    String company;
+    double cost;
+    public double getCost()
+    {
+        return cost;
+    }
+
+    public void setCost(double cost)
+    {
+        cost = this.cost;
+    }
+
+    public String getCompany()
+    {
+        return company;
+    }
+
+    public void setCompany(String company)
+    {
+        this.company = company;
+    }
+
+    @Override
+    public String importOrigin()
+    {
+        return "Japan";
+    }
+
+    @Override
+    public double cost()
+    {
+        return cost;
+    }
+    @Override
+    public String toString()
+    {
+        return "This is Engine object from: "
+                + company;
+    }
+}
---
+++ DependencyInjectionSpring/src/main/java/com/JEE/interfaces/IEngine.java
@@ -0,0 +1,7 @@
+package com.JEE.interfaces;
+
+public interface IEngine {
+    String EMISSION_NORMS = "BSIV";
+    String importOrigin();
+    double cost();
+}
